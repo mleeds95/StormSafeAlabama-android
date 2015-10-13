4b1393c7c0c3bc354f92818ee1964b855e25e8c7
Matthew Leeds <mwl458@gmail.com>
2015-10-12 19:00:03 -0500

    Add Google's copyright notice.

--- app/src/main/java/mwleeds/stormsafealabama/CopyrightNotice.java
+++ app/src/main/java/mwleeds/stormsafealabama/CopyrightNotice.java
@@ -3,6 +3,10 @@ package mwleeds.stormsafealabama;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class CopyrightNotice extends AppCompatActivity {
 
@@ -14,6 +18,18 @@ public class CopyrightNotice extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView tv = (TextView) findViewById(R.id.copyright_text);
+        tv.setText("");
+        // Add a copyright notice for this app's code.
+        tv.append(getString(R.string.app_copyright));
+        // Add a copyright notice for the app's icon.
+        tv.append(getString(R.string.icon_copyright));
+        // Add the required copyright notice for the Google Play Services API
+        String googleLicenseInfo = GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(this);
+        if (googleLicenseInfo != null) {
+            tv.append(googleLicenseInfo);
+        }
     }
 
 }
