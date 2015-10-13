8ee0c656e69f9c9e8fe147cb6156801e5d3aceb8
Matthew Leeds <mwl458@gmail.com>
2015-10-12 23:43:54 -0500

    Prompt the user to enable location services if necessary.

--- app/src/main/java/mwleeds/stormsafealabama/MapsActivity.java
+++ app/src/main/java/mwleeds/stormsafealabama/MapsActivity.java
@@ -1,5 +1,8 @@
 package mwleeds.stormsafealabama;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -7,9 +10,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.games.GamesActivityResultCodes;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,9 +33,12 @@ public class MapsActivity extends FragmentActivity
     private GoogleMap mMap;
     private GoogleApiClient mLocationClient;
     private LocationListener mLocationListener;
+    private LocationRequest mLocationRequest;
+    private boolean locationSettingsGood = false;
     public static final long GPS_LOCATION_INTERVAL = 5000;
     public static final long GPS_FASTEST_INTERVAL = 1000;
     public static final int MAP_ZOOM_LEVEL = 15;
+    public static final int REQUEST_CHECK_SETTINGS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +77,7 @@ public class MapsActivity extends FragmentActivity
 
     @Override
     public void onConnected(Bundle bundle) {
+
         // implement the LocationListener interface to update the map for each change
         mLocationListener = new LocationListener() {
             @Override
@@ -72,12 +86,67 @@ public class MapsActivity extends FragmentActivity
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, MAP_ZOOM_LEVEL));
             }
         };
+
         // set the time interval for location updates
-        LocationRequest request = LocationRequest.create();
-        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        request.setInterval(GPS_LOCATION_INTERVAL);
-        request.setFastestInterval(GPS_FASTEST_INTERVAL);
-        LocationServices.FusedLocationApi.requestLocationUpdates(mLocationClient, request, mLocationListener);
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(GPS_LOCATION_INTERVAL);
+        mLocationRequest.setFastestInterval(GPS_FASTEST_INTERVAL);
+
+        // Check if the user's location services is enabled
+        LocationSettingsRequest.Builder locationSettingsBuilder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi
+                .checkLocationSettings(mLocationClient, locationSettingsBuilder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult locationSettingsResult) {
+                final Status status = locationSettingsResult.getStatus();
+                switch(status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        locationSettingsGood = true;
+                        // request regular location updates
+                        LocationServices.FusedLocationApi.requestLocationUpdates(mLocationClient, mLocationRequest, mLocationListener);
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // open a dialog asking the user to fix their location settings
+                        try {
+                            status.startResolutionForResult(MapsActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            Toast.makeText(MapsActivity.this, R.string.error_fix_location_settings, Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // inform the user they have no means of finding their location
+                        Toast.makeText(MapsActivity.this, R.string.bad_location_settings, Toast.LENGTH_SHORT).show();
+                        locationSettingsGood = false;
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        locationSettingsGood = true;
+                        // request regular location updates
+                        LocationServices.FusedLocationApi.requestLocationUpdates(mLocationClient, mLocationRequest, mLocationListener);
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        // inform the user they won't be able to get their location
+                        Toast.makeText(MapsActivity.this, R.string.location_settings_unchanged, Toast.LENGTH_SHORT).show();
+                        locationSettingsGood = false;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
     }
 
     @Override
